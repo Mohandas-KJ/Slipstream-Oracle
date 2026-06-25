75aa1ec11f03ad66444572b2ad0f7825328a60cf
Mohan Das <mohandaskj.11@gmail.com>
2026-06-25 18:13:51 +0530

    Add initial version of oracle_v1_pickle.pkl to outputs directory

--- features/build_training_dataset.py
+++ features/build_training_dataset.py
@@ -1,6 +1,7 @@
 # Import
 import pandas as pd
 from pathlib import Path
+import numpy as np
 import fastf1
 import sys
 
@@ -29,10 +30,21 @@ def get_event_list(year):
 
     fastf1.Cache.enable_cache("cache")
 
-    event = fastf1.get_event_schedule(year,include_testing=False)
-    fin_list = event.EventName.to_list()
-    for i in fin_list:
-        fin_list[fin_list.index(i)] = str(i).replace(" ","_")
+    event = fastf1.get_event_schedule(
+        year,
+        include_testing=False
+    )
+
+    # Keep only completed events
+    event = event[
+        event["EventDate"] < pd.Timestamp.now()
+    ]
+
+    fin_list = (
+        event["EventName"]
+        .str.replace(" ", "_")
+        .tolist()
+    )
 
     return fin_list
 
@@ -48,12 +60,34 @@ for y in YEAR:
     events[y] = get_event_list(y)
     drivers[y] =  get_drivers_GP(y)
 
-print(drivers)
-
 for dt in YEAR:
     for eve in events[dt]:
-        quali_res = DATA_DIR / str(dt) / eve / Q_RESULT
-        race_res = DATA_DIR / str(dt) / eve / R_RESULT
+        quali_res = pd.read_csv(DATA_DIR / str(dt) / eve / Q_RESULT)
+        race_res = pd.read_csv(DATA_DIR / str(dt) / eve / R_RESULT)
+
+        for driv in drivers[dt]:
+            df["Year"].append(dt)
+            df["Race"].append(eve)
+            df["Driver"].append(driv)
+
+            if driv in quali_res["Abbreviation"].values and driv in race_res["Abbreviation"].values:
+                df["Team"].append(quali_res[quali_res["Abbreviation"] == driv]["TeamName"].iloc[0])
+                df["QualiPosition"].append(quali_res[quali_res["Abbreviation"] == driv]["Position"].iloc[0])
+                df["GridPosition"].append(race_res[race_res["Abbreviation"] == driv]["GridPosition"].iloc[0])
+                df["TargetFinish"].append(race_res[race_res["Abbreviation"] == driv]["ClassifiedPosition"].iloc[0])
+                df["Points"].append(race_res[race_res["Abbreviation"] == driv]["Points"].iloc[0])
+            else:
+                df["Team"].append(np.nan)
+                df["QualiPosition"].append(np.nan)
+                df["GridPosition"].append(np.nan)
+                df["TargetFinish"].append(np.nan)
+                df["Points"].append(np.nan)
+
+dataframe = pd.DataFrame(df)
+dataframe.to_csv("outputs/oracle_v1.csv")
+print("CSV Exported Successfully!")
+dataframe.to_pickle("outputs/oracle_v1_pickle.pkl")
+print("Pickle File Generated!")
 
 
 
