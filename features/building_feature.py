e08a916843024383364465d0971f162359fd2fa9
Mohan Das <mohandaskj.11@gmail.com>
2026-06-27 09:48:12 +0530

    Add feature extraction for race data in extract_feature_35 function

--- features/building_feature.py
+++ features/building_feature.py
@@ -27,7 +27,20 @@ def add_round_number(df: pd.DataFrame) -> pd.DataFrame:
 
 def extract_feature_35(year):
 
-    new_df = {}
+    new_df = {"Year": [],
+              "RoundNumber": [],
+              "Race": [],
+              "Driver": [],
+              "Team": [],
+              "QualiPosition": [],
+              "GridPosition": [],
+              "AvgFinishLast3": [],
+              "AvgFinishLast5": [],
+              "AvgGridLast3": [],
+              "AvgGridLast5": [],
+              "AvgPointsLast3": [],
+              "AvgPointsLast5": [],
+              "TargetFinish": []}
 
     df = pd.read_csv("outputs/oracle_v1.csv")
     df = add_round_number(df).drop(columns=["Unnamed: 0"]).sort_values(by=["Year","Round"],ascending=True).reset_index(drop=True)
@@ -41,15 +54,27 @@ def extract_feature_35(year):
     last5 = target_data["Round"].unique()[::-1][:5].tolist()
 
     for r in rounds:
-        new_df["Year"] = year
-        new_df["RoundNumber"] = r
-        new_df["Race"] = target_data[target_data["Round"] == r]["Race"].unique()
-
         for d in drivers:
-            new_df["Driver"] = d
-            
+
+            new_df["Year"].append(year)
+            new_df["RoundNumber"].append(r)
+            new_df["Race"].append(target_data[target_data["Round"] == r]["Race"].iloc[0])
+
+            new_df["Driver"].append(d)
+            new_df["Team"].append(target_data[target_data["Driver"] == d]["Team"].iloc[0])
+            new_df["QualiPosition"].append(target_data[(target_data["Round"] == r) & (target_data["Driver"] == d)]["QualiPosition"].iloc[0])
+            new_df["GridPosition"].append(target_data[(target_data["Round"] == r) & (target_data["Driver"] == d)]["GridPosition"].iloc[0])
+            new_df["AvgFinishLast3"].append(pd.to_numeric(target_data[(target_data["Round"].isin(last3)) &(target_data["Driver"] == d)]["TargetFinish"],errors="coerce").mean().round(2))
+            new_df["AvgFinishLast5"].append(pd.to_numeric(target_data[(target_data["Round"].isin(last5)) &(target_data["Driver"] == d)]["TargetFinish"],errors="coerce").mean().round(2))
+            new_df["AvgGridLast3"].append(pd.to_numeric(target_data[(target_data["Round"].isin(last3)) &(target_data["Driver"] == d)]["GridPosition"],errors="coerce").mean().round(2))
+            new_df["AvgGridLast5"].append(pd.to_numeric(target_data[(target_data["Round"].isin(last5)) &(target_data["Driver"] == d)]["GridPosition"],errors="coerce").mean().round(2))
+            new_df["AvgPointsLast3"].append(pd.to_numeric(target_data[(target_data["Round"].isin(last3)) &(target_data["Driver"] == d)]["Points"],errors="coerce").mean().round(2))
+            new_df["AvgPointsLast5"].append(pd.to_numeric(target_data[(target_data["Round"].isin(last5)) &(target_data["Driver"] == d)]["Points"],errors="coerce").mean().round(2))
+            new_df["TargetFinish"].append(target_data[(target_data["Round"].isin(last3)) &(target_data["Driver"] == d)]["TargetFinish"].iloc[0])
 
     
+    df = pd.DataFrame(new_df)
+    df.to_csv("outputs/oracle_v2.csv",index=False)
 
 extract_feature_35(2026)
 
