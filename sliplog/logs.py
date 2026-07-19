f63a15bc1bc9fd7ac54bd3a0f33ec0f60e91f425
Mohan Das <mohandaskj.11@gmail.com>
2026-07-19 09:09:44 +0530

    Add logging functionality and update race data for British GP 2026

--- sliplog/logs.py
+++ sliplog/logs.py
@@ -6,4 +6,4 @@ from Generals import streamlib
 def write(txt):
     cur_gp = streamlib.get_current_gp_no()
     with open("Logs/Sliplog.log","a",encoding="utf-8") as logf:
-        logf.write(f"[{datetime.now().strftime("%d-%m-%Y %H:%M:%S")}][{cur_gp - streamlib.get_eventname(cur_gp)}] - {txt}\n")
+        logf.write(f"[{datetime.now().strftime("%d-%m-%Y %H:%M:%S")}][{cur_gp} - {streamlib.get_eventname(cur_gp)}] - {txt}\n")
