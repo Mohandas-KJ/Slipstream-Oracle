b7db8e6165c038f9bd6c39827059f9c38b991fbc
Mohan Das <mohandaskj.11@gmail.com>
2026-07-19 08:50:15 +0530

    Enhance logging format to include current Grand Prix number and event name

--- sliplog/logs.py
+++ sliplog/logs.py
@@ -1,7 +1,9 @@
 # Imports
 from datetime import datetime
+from Generals import streamlib
 
 # Code for Logging
 def write(txt):
+    cur_gp = streamlib.get_current_gp_no()
     with open("Logs/Sliplog.log","a",encoding="utf-8") as logf:
-        logf.write(f"[{datetime.now().strftime("%d-%m-%Y %H:%M:%S")}] - {txt}\n")
+        logf.write(f"[{datetime.now().strftime("%d-%m-%Y %H:%M:%S")}][{cur_gp - streamlib.get_eventname(cur_gp)}] - {txt}\n")
